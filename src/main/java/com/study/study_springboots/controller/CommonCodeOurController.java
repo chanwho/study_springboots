d2329eee4ba5c0a35a5954ffdbdd25343d85ff45
chanwho <anxi0015@gmail.com>
2023-01-19 11:54:48 +0900

    reset project and reOperation 갑자기 실행이 안되는 오류발생. 다시 프로젝트 받아와서 완성

--- src/main/java/com/study/study_springboots/controller/CommonCodeOurController.java
+++ src/main/java/com/study/study_springboots/controller/CommonCodeOurController.java
@@ -20,29 +20,38 @@ public class CommonCodeOurController {
     @Autowired
     CommonCodeOurService commonCodeOurService;
 
-    @RequestMapping(value = {"/form"}, method = RequestMethod.GET)
+    @RequestMapping(value = { "/insert" }, method = RequestMethod.POST)
+    public ModelAndView insert(@RequestParam Map<String, Object> params, ModelAndView modelAndView) {
+        commonCodeOurService.insertOne(params);
+        modelAndView.setViewName("commonCode_our/list");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = { "/form" }, method = RequestMethod.GET)
     public ModelAndView form(@RequestParam Map<String, Object> params, ModelAndView modelAndView) {
         modelAndView.setViewName("commonCode_our/edit");
         return modelAndView;
     }
 
-    @RequestMapping(value = {"/delete/{uniqueId}"}, method = RequestMethod.GET)
-    public ModelAndView delete(@RequestParam Map<String, Object> params, @PathVariable String uniqueId
-                    , ModelAndView modelAndView) {
+    @RequestMapping(value = { "/delete/{uniqueId}" }, method = RequestMethod.POST)
+    public ModelAndView delete(@RequestParam Map<String, Object> params, @PathVariable String uniqueId,
+            ModelAndView modelAndView) {
         params.put("COMMON_CODE_ID", uniqueId);
-        commonCodeOurService.delete(params);
+        Object resultMap = commonCodeOurService.deleteAndGetList(params);
+        modelAndView.addObject("resultMap", resultMap);
         modelAndView.setViewName("commonCode_our/list");
         return modelAndView;
     }
 
-    @RequestMapping(value = {"/update"}, method = RequestMethod.POST)
+    @RequestMapping(value = { "/update" }, method = RequestMethod.POST)
     public ModelAndView update(@RequestParam Map<String, Object> params, ModelAndView modelAndView) {
-        commonCodeOurService.updateOne(params);
+        Object resultMap = commonCodeOurService.updateAndGetList(params);
+        modelAndView.addObject("resultMap", resultMap);
         modelAndView.setViewName("commonCode_our/list");
         return modelAndView;
     }
 
-    @RequestMapping(value = {"/list", "/", ""}, method = RequestMethod.GET)
+    @RequestMapping(value = { "/list", "/", "" }, method = RequestMethod.GET)
     public ModelAndView list(@RequestParam Map<String, Object> params, ModelAndView modelAndView) {
         Object resultMap = commonCodeOurService.getList(params);
         modelAndView.addObject("resultMap", resultMap);
@@ -50,9 +59,10 @@ public class CommonCodeOurController {
         return modelAndView;
     }
 
-    @RequestMapping(value = {"/edit/{uniqueId}"}, method = RequestMethod.GET)
-    public ModelAndView edit(@RequestParam Map<String, Object> params, @PathVariable String uniqueId
-                    , ModelAndView modelAndView) {
+    @RequestMapping(value = { "/edit/{uniqueId}" }, method = RequestMethod.GET)
+    public ModelAndView edit(@RequestParam Map<String, Object> params, @PathVariable String uniqueId,
+            ModelAndView modelAndView) {
+
         params.put("COMMON_CODE_ID", uniqueId);
         Object resultMap = commonCodeOurService.getOne(params);
         modelAndView.addObject("resultMap", resultMap);
@@ -60,5 +70,4 @@ public class CommonCodeOurController {
         return modelAndView;
     }
 
-    
 }
--- src/main/java/com/study/study_springboots/service/CommonCodeOurService.java
+++ src/main/java/com/study/study_springboots/service/CommonCodeOurService.java
@@ -10,30 +10,45 @@ public class CommonCodeOurService {
     @Autowired
     CommonCodeOurDao commonCodeOurDao;
 
-    public Object getList(Object dataMap){
+    public Object deleteAndGetList(Object dataMap) {
+        Object result = this.delete(dataMap);/* 이미 이 클래스 내에서 delete가 있기 때문 */
+        result = this.getList(dataMap);
+        return result;
+    }
+
+    public Object updateAndGetList(Object dataMap) {
+        Object result = this.update(dataMap);/* 이미 이 클래스 내에서 delete가 있기 때문 */
+        result = this.getList(dataMap);
+        return result;
+    }
+
+    public Object getList(Object dataMap) {
         String sqlMapId = "CommonCodeOur.selectListByUID";
         Object result = commonCodeOurDao.getList(sqlMapId, dataMap);
         return result;
     }
 
-    public Object getOne(Object dataMap){
+    public Object getOne(Object dataMap) {
         String sqlMapId = "CommonCodeOur.selectByUID";
-
         Object result = commonCodeOurDao.getOne(sqlMapId, dataMap);
         return result;
     }
 
-    public Object updateOne(Object dataMap){
+    public Object update(Object dataMap) {
         String sqlMapId = "CommonCodeOur.updateByUID";
-
         Object result = commonCodeOurDao.update(sqlMapId, dataMap);
-        return result;    
+        return result;
     }
 
-    public Object delete(Object dataMap){
-        String sqlMapId = "CommonCodeOur.deleteByUID";
+    public Object insertOne(Object dataMap) {
+        String sqlMapId = "CommonCodeOur.insertWithUID";
+        Object result = commonCodeOurDao.insert(sqlMapId, dataMap);
+        return result;
+    }
 
+    public Object delete(Object dataMap) {
+        String sqlMapId = "CommonCodeOur.deleteByUID";
         Object result = commonCodeOurDao.delete(sqlMapId, dataMap);
-        return result;    
+        return result;
     }
 }
