7fb0afdd979d9343e80f502b2b686eac38cccc50
chanwho <anxi0015@gmail.com>
2023-01-26 15:34:54 +0900

    way to upload 2 files

--- src/main/java/com/study/study_springboots/dao/AttachFileDao.java
+++ src/main/java/com/study/study_springboots/dao/AttachFileDao.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CommonCodeOurDao {
+public class AttachFileDao {
     @Autowired
     private SqlSessionTemplate sqlSessionTemplate;
 
